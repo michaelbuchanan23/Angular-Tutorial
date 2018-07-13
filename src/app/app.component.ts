c817beebbae034b3371367ae55c85a050fe5deb9
michaelbuchanan23 <michaelbuchanan23@gmail.com>
2018-07-13 16:02:06 -0400

    initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,10 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+  title = 'Angular Tutorial';
+  sometext='my text';
+
+  onclick(): void {
+  	this.sometext = "This is some text";
+  }
 }
